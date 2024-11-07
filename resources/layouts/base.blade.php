bb7351b43916e2b73650355d654b031d19677043
Laurens <79937870+laurensvt09@users.noreply.github.com>
2024-11-07 09:51:40 +0100

    layout gemaakt voor het project

---
+++ resources/layouts/base.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Barroc intens</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+
+    @include('components.navbar')
+
+    <main class="container mt-4">
+        @yield('content')
+    </main>
+
+    @include('components.footer')
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-light text-center text-lg-start mt-5 py-3">
+    <div class="container">
+
+    </div>
+</footer>

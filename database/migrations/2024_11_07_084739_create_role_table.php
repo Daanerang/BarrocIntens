bf50c97383e9c8e54768981be5abe98b08c2549c
D1L4N0 <dqdebie@gmail.com>
2024-11-07 10:12:19 +0100

    alle migrations als het goed is gefixed
    migrations volgorde stond verkeed en heb dit aangepast

--- database/migrations/2024_11_07_084739_create_role_table.php
+++ database/migrations/2024_11_07_084739_create_role_table.php
@@ -4,19 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRoleTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('role', function (Blueprint $table) {
-            $table->id('RolId');
-            $table->string('Naam');
+            $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('role');
     }
-}
+};
--- database/migrations/2024_11_07_090220_create_part_table.php
+++ database/migrations/2024_11_07_090220_create_part_table.php
@@ -4,20 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePartTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('part', function (Blueprint $table) {
-            $table->id('PartId');
-            $table->string('Name');
-            $table->integer('Stock');
+            $table->id();
+            $table->string('name');
+            $table->integer('stock');
             $table->timestamps();
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('part');
     }
-}
+};
